945c5d0997060ead6b03bf8a4926146becfab86e
Aviral <36530173+aviral17@users.noreply.github.com>
2025-04-05 17:46:36 +0530

    Updated config.rs

--- crates/axum-server/src/config.rs
+++ crates/axum-server/src/config.rs
@@ -4,6 +4,7 @@ pub struct Config {
 }
 // using Clone to retain the ownership of Config struct as we are returning Clone Debug in impl
 
+// Updated it as per latest changes
 impl Config {
     pub fn new() -> Config {
         let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not set");
