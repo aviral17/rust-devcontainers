67e63d6c689b97b6be95d0440a7ee898f20997c7
Aviral <36530173+aviral17@users.noreply.github.com>
2025-04-07 23:59:32 +0530

    updated build.rs

--- crates/db/build.rs
+++ crates/db/build.rs
@@ -16,6 +16,7 @@ fn cornucopia() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let file_path = Path::new(&out_dir).join("cornucopia.rs");
 
+    // update it
     let db_url = env::var_os("DATABASE_URL").unwrap();
 
     // Rerun this build script if the queries or migrations change.
